a431ae487649c8eddca0e8843788258cb3b5e6a5
Sibaprasad Maiti <sibu.it13@gmail.com>
2023-08-17 21:56:03 +0530

    validate_add_bal_transaction unit test added

--- src/validators/add_bal.rs
+++ src/validators/add_bal.rs
@@ -10,6 +10,9 @@ pub async fn validate_add_bal_transaction(
     user_id: u32,
     body: &AddBalEndReq,
 ) -> Result<(), AppError> {
+    if !body.is_successful {
+        return Ok(());
+    }
     let filter = doc! {
         "_id": &body.transaction_id,
         "userId": user_id,
@@ -49,4 +52,168 @@ pub async fn validate_add_bal_transaction(
         return Err(err);
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+
+    use anyhow::anyhow;
+    use mockall::predicate::{eq, function};
+    use mongodb::bson::oid::ObjectId;
+
+    use crate::config::AppState;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_validate_add_bal_transaction() {
+        let amount = 10;
+        let mut req = AddBalEndReq {
+            amount: amount,
+            transaction_id: ObjectId::new(),
+            is_successful: false,
+            error_reason: None,
+            tracking_id: None,
+        };
+        let user_id = 5;
+        let mut state = AppState::mock();
+        // scenario is_successful = false then this function returns Ok immediately
+        let wallet_helpers = state.get_mut_helpers().mut_wallet_helpers();
+        wallet_helpers.expect_get_wallet_transaction().never();
+        wallet_helpers.expect_get_user_balance().never();
+        let result = validate_add_bal_transaction(state.db(), state.helpers(), user_id, &req).await;
+        assert_eq!(result.is_ok(), true);
+
+        req.is_successful = true;
+
+        // scenario - get_wallet_transaction function return Err
+        let wallet_helpers = state.get_mut_helpers().mut_wallet_helpers();
+        wallet_helpers
+            .expect_get_wallet_transaction()
+            .once()
+            .returning(|_, _| Err(anyhow!("some error")));
+        wallet_helpers
+            .expect_get_user_balance()
+            .once()
+            .with(function(|db: &DbClient| true), eq(user_id))
+            .returning(|_, _| Ok(Money::default()));
+        let result = validate_add_bal_transaction(state.db(), state.helpers(), user_id, &req).await;
+        match result {
+            Err(AppError::AnyError(e)) => assert_eq!(e.to_string(), "some error"),
+            _ => panic!(),
+        };
+
+        // scenario WalletTransaction not found
+        let wallet_helpers = state.get_mut_helpers().mut_wallet_helpers();
+        wallet_helpers
+            .expect_get_wallet_transaction()
+            .once()
+            .returning(|_, _| Ok(None));
+        wallet_helpers
+            .expect_get_user_balance()
+            .once()
+            .with(function(|db: &DbClient| true), eq(user_id))
+            .returning(|_, _| Ok(Money::default()));
+        let result = validate_add_bal_transaction(state.db(), state.helpers(), user_id, &req).await;
+        match result {
+            Err(AppError::NotFound(e)) => assert_eq!(e, "transaction not found"),
+            _ => panic!(),
+        };
+
+        // scenario get_user_balance return Err
+        let wallet_helpers = state.get_mut_helpers().mut_wallet_helpers();
+        wallet_helpers
+            .expect_get_wallet_transaction()
+            .once()
+            .returning(|_, _| Ok(Some(WalletTransaction::default())));
+        wallet_helpers
+            .expect_get_user_balance()
+            .once()
+            .with(function(|db: &DbClient| true), eq(user_id))
+            .returning(|_, _| Err(anyhow!("some error")));
+        let result = validate_add_bal_transaction(state.db(), state.helpers(), user_id, &req).await;
+        match result {
+            Err(AppError::AnyError(e)) => assert_eq!(e.to_string(), "some error"),
+            _ => panic!(),
+        };
+
+        // scenario amount do not match
+        let wallet_helpers = state.get_mut_helpers().mut_wallet_helpers();
+        wallet_helpers
+            .expect_get_wallet_transaction()
+            .once()
+            .returning(|_, _| Ok(Some(WalletTransaction::default())));
+        wallet_helpers
+            .expect_get_user_balance()
+            .once()
+            .with(function(|db: &DbClient| true), eq(user_id))
+            .returning(|_, _| Ok(Money::default()));
+        wallet_helpers.expect_update_failed_transaction().never();
+        let result = validate_add_bal_transaction(state.db(), state.helpers(), user_id, &req).await;
+        match result {
+            Err(AppError::BadRequest(e)) => assert_eq!(e.to_string(), "amount do not match"),
+            _ => panic!(),
+        };
+
+        // scenario user balance do not match and error
+        let transaction_id = req.transaction_id.clone();
+        let error_reason = req.error_reason.clone();
+        let tracking_id = req.tracking_id.clone();
+        let wallet_helpers = state.get_mut_helpers().mut_wallet_helpers();
+        wallet_helpers
+            .expect_get_wallet_transaction()
+            .once()
+            .returning(move |_, _| {
+                Ok(Some(WalletTransaction::add_bal_init_trans(
+                    user_id,
+                    amount,
+                    Money::default(),
+                )))
+            });
+        wallet_helpers
+            .expect_get_user_balance()
+            .once()
+            .with(function(|db: &DbClient| true), eq(user_id))
+            .returning(|_, _| Ok(Money::new(3, 0)));
+        wallet_helpers
+            .expect_update_failed_transaction()
+            .once()
+            .with(
+                function(|db: &DbClient| true),
+                eq(user_id),
+                eq(transaction_id),
+                eq(error_reason),
+                eq(tracking_id),
+            )
+            .return_once(|_, _, _, _, _| Ok(()));
+        let result = validate_add_bal_transaction(state.db(), state.helpers(), user_id, &req).await;
+        match result {
+            Err(AppError::BadRequest(e)) => assert_eq!(
+                e.contains("does not match with transaction balanceBefore"),
+                true
+            ),
+            _ => panic!(),
+        };
+
+        // user balance matched and success
+        let wallet_helpers = state.get_mut_helpers().mut_wallet_helpers();
+        wallet_helpers
+            .expect_get_wallet_transaction()
+            .once()
+            .returning(move |_, _| {
+                Ok(Some(WalletTransaction::add_bal_init_trans(
+                    user_id,
+                    amount,
+                    Money::new(3, 0),
+                )))
+            });
+        wallet_helpers
+            .expect_get_user_balance()
+            .once()
+            .with(function(|db: &DbClient| true), eq(user_id))
+            .returning(|_, _| Ok(Money::new(3, 0)));
+        wallet_helpers.expect_update_failed_transaction().never();
+        let result = validate_add_bal_transaction(state.db(), state.helpers(), user_id, &req).await;
+        assert_eq!(result.is_ok(), true);
+    }
 }
